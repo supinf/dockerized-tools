3d258611f892645c54d454813b4d78229432aaf8
ryo nakamaru <pottava@gmail.com>
2017-02-07 13:39:30 +0900

    Add swagger-mock-api definitions

---
+++ nodejs/swagger-mock-api/versions/1.6/Gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var path = require('path');
+var mockApi = require('swagger-mock-api');
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          middleware: [
+            function (req, res, next) {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+                res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
+                res.setHeader("Access-Control-Max-Age", "86400");
+                if (req.method == 'OPTIONS') {
+                    res.end('ok!');
+                } else {
+                    next();
+                }
+            },
+            mockApi({
+                swaggerFile: '/api/swagger.yaml',
+                watch: true
+            })
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default', ['connect']);
+};
