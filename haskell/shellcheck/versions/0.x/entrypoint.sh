ecbe05ea2a907601d633b137873e5e4341d03c76
ryo nakamaru <pottava@gmail.com>
2018-04-15 22:00:36 +0900

    shellcheck with `IgnorePath` option

--- haskell/shellcheck/versions/0.x/entrypoint.sh
+++ haskell/shellcheck/versions/0.x/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 rc=0
-for file in $( find . -type f -name "*.sh" -print | sed 's/^\.\///' ); do
-  shellcheck -f tty "$file" || rc=$?
-done
+
+if [ "x${IGNORE_PATH}" = "x" ]; then
+  for file in $( find . -type f -name "*.sh" -print | sed 's/^\.\///' ); do
+    shellcheck -f tty "$file" || rc=$?
+  done
+else
+  for file in $( find . -type f -name "*.sh" -print | sed 's/^\.\///' | grep -v "${IGNORE_PATH}" ); do
+    shellcheck -f tty "$file" || rc=$?
+  done
+fi
+
 exit $rc
