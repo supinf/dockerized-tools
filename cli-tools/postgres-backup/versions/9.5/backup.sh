4b98435c24ea0b36013693d8119878ede8b38ee0
ryo nakamaru <pottava@gmail.com>
2018-09-10 14:19:47 +0900

    Backup database with custom KMS Key

--- cli-tools/postgres-backup/versions/9.5/backup.sh
+++ cli-tools/postgres-backup/versions/9.5/backup.sh
@@ -11,6 +11,15 @@ echo "Uploading the dump to ${S3_BUCKET}"
 
 now=$(date +"%Y-%m-%dT%H:%M:%SZ")
 key="s3://${S3_BUCKET}/${S3_PREFIX}${POSTGRES_DATABASE}_${now}.sql.gz"
-aws s3 cp dump.sql.gz "${key}" || exit 2
+
+if [ "${SERVER_SIDE_ENCRYPTION}" = "true" ]; then
+  if [ "x${KMS_KEY_ID}" = "x" ]; then
+    aws s3 cp --sse AES256 dump.sql.gz "${key}" || exit 2
+  else
+    aws s3 cp --sse aws:kms --sse-kms-key-id "${KMS_KEY_ID}" dump.sql.gz "${key}" || exit 2
+  fi
+else
+  aws s3 cp dump.sql.gz "${key}" || exit 2
+fi
 
 echo "Uploaded successfully"
